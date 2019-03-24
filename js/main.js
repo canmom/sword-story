3ddf362eefecc931a79173f3f98d8a8f5dc6cffb
canmom <bryn.dickinson@gmail.com>
2019-03-24 14:24:45 +0000

    Initial commit
    Contains animated text display and basic stylings, and first
    version of the story script.

---
+++ js/main.js
@@ -0,0 +1,92 @@
+const game = {
+  narration : document.getElementById('narration'),
+  currentParagraphs : document.getElementById('passages').childNodes,
+  paragraphIndex : 0,
+  passageIndex : 0,
+  passageContainer: document.getElementById('passages'),
+  narrationBox: document.getElementById('narration')
+};
+
+function smarten(text) {
+  return text
+    /* opening singles */
+    .replace(/(^|[\s(\["])'/g, "$1‘")
+
+    /* closing singles & apostrophes */
+    .replace(/'/g, "’")
+
+    /* opening doubles */
+    .replace(/(^|[/\[(\u2018\s])"/g, "$1“")
+
+    /* closing doubles */
+    .replace(/"/g, "”")
+
+    /* em-dashes */
+    .replace(/--/g, "\u2014");
+};
+
+function recursivelySmarten(element) {
+  element.childNodes.forEach( (child) => {
+    if (child.nodeType === Node.ELEMENT_NODE) {
+      recursivelySmarten(child)
+    } else if (child.nodeType === Node.TEXT_NODE) {
+        child.nodeValue = smarten(child.nodeValue);
+    }
+  });
+}
+
+function renderPassage(passage) {
+  game.passageContainer.innerHTML = marked(passage);
+
+  game.passageContainer.childNodes.forEach((element, index) => {
+    if (element.nodeType === Node.TEXT_NODE) {
+      element.remove()
+    } else {
+      if (index === 0) {
+        element.classList.add('revealed');
+      }
+      recursivelySmarten(element);
+    }
+  });
+
+  game.paragraphIndex = 0;
+}
+
+function hideNarration() {
+  game.narrationBox.classList.add('disappearing');
+}
+
+function revealNarration() {
+  game.narrationBox.classList.remove('disappearing');
+}
+
+function displayPassage() {
+  renderPassage(passages[game.passageIndex]);
+  console.log('DisplayPassage called')
+  game.passageIndex += 1;
+}
+
+function nextPassage() {
+  hideNarration();
+
+  //when the fadeout is complete...
+  game.narrationBox.addEventListener('transitionend', function (e) {
+    game.narrationBox.removeEventListener('transitionend', arguments.callee);
+    console.log('Event caught:' + e)
+    displayPassage();
+    revealNarration();
+  });
+}
+
+function showNextParagraph() {
+  game.paragraphIndex += 1;
+  if (game.paragraphIndex < game.currentParagraphs.length) {
+    game.currentParagraphs.item(game.paragraphIndex).classList.add('revealed');;
+  } else {
+    nextPassage();
+  }
+}
+
+document.getElementById('nextbutton').addEventListener('click', showNextParagraph)
+
+displayPassage();
---
+++ js/story.js
@@ -0,0 +1,280 @@
+const passages = [`When you open your eyes for the first time after your death, you know exactly where you are.
+
+How you came to be here...
+
+Well, that's another question.`,
+
+`Around you, idle quintessence assumes half-forgotten forms - images stamped on it by centuries, that not even your hammer could squeeze out.
+
+An architectural cacophony, visual languages tangled and knotted, the wills of countless forgotten builders lashed together with silver threads.
+
+(The prison you built.)`,
+
+`It's too much.`,
+
+`No-sound stuffs your ears, bile rises in your throat, your thoughts are mush...
+
+Desperately, you reach for the creed.`,
+
+`*Breathe.*
+
+Think of iron.
+
+Think of the red blood of the Earth, hammered and shaped by human will.
+
+Think of the Alchemist's art and the Smith's determination, unified in one great work.`,
+
+`You can take some small comfort from the fact that your great work has indeed come to fruition.
+
+For where else can you be, but the heart of your sword?`,
+
+`In the first month, you fed the sword with your blood.
+
+(Not much of a sacrifice, you already knew. You already shed your own blood far too willingly.)
+
+In the second month, you fed the sword with bitter tears.
+
+(They came easily; your husband fell to the Rosers, and too many others to name.)`,
+
+`In the third month, you fed the sword with fluids you'd rather not name.
+
+(How coy! Someone made a joke about fucking the sword, and you thought, it's worth a shot...)
+
+In the fourth month, you took your husband's ashes, and fed them to the sword.
+
+(Not three months since he held you in his arms! Would this sacrifice at last be sufficient?)`,
+
+`(It was not.)`,
+
+`In the fifth month, you reduced your own legs to *quintessence*, and they too entered the forge.
+
+(Somewhat gauche, in my view. But you were running out of ideas.)
+
+You remember the first day of the sixth month...`,
+
+`The Princess gripped your shoulders, her too-long, many-jointed fingers folding around to dig into your shoulder blades.
+
+"Master Smith! The Rosers stand at our gates. I beg of you - is the weapon ready? Can we yet be saved?"`,
+
+`You remember the throne room, gloomy, emptied of courtiers. Even the Princess's firewood was needed for the war effort, now.
+
+You remember the shouts of the rioters outside. Begging for a way to eat, or a way to leave, or a way to fight, or just a way to die.`,
+
+`"My Princess: I will do my duty. The weapon will be ready. I will sacrifice anything - even my life."
+
+Well, it seems like you did.
+
+(Congratulations.)`,
+
+`Belatedly, you become aware of the voice. It is so hard to tell it apart from your own thoughts.
+
+But it's familiar, so familiar...
+
+Guilt hits you like a hammer.`,
+
+`Breathe!
+
+Think of iron!
+
+Think of the red blood of the Earth--
+
+(But it's not the *Earth*'s blood you're thinking of, is it?)
+
+Think of the red blood of the Earth, hammered and shaped by human will.
+
+Think of the Alchemist's art and the Smith's determination, unified in one great work!
+
+Think of all that you have sacrificed.`,
+
+`(Think of the sacrifice you even know refuse to acknowledge...)`,
+
+`(Think of me.)`,
+
+`You call out. Your voice does not echo.
+
+(I'm already here, Master Smith. Serving you faithfully, always and forever.)
+
+(This is what you wanted, isn't it?)`,
+
+`A night. Not so very long ago.
+
+The Princess's words rang in your ears. There was no time left.
+
+You made for the forge, still unsteady on your new legs. The furnace would still be hot. Nura would be tending it, dutifully practicing the Art.
+
+Your husband had laughed when you'd announced you had chosen a homunculus as your apprentice.
+
+"I know you're given to charity, my dear, but that's a bit much, isn't it?"`,
+
+`Nura looked up as you entered. Her porcelain jaw clicked into a slightly sardonic smile as she recognised you.
+
+"Left your keys again, Master?"
+
+Her hammer struck the piece on the anvil at a precise angle, sending sparks dancing across her soot-coated, porcelain hands.
+
+You can't imagine where she got this body. Few homunculi could dream of affording its like.`,
+
+`You said nothing. She tilted her head, watching you.
+
+"Have I erred in some way?" she ventured, after a pause.
+
+"No, Azinureta. You have ever been a faithful, true apprentice. You are skilled in our Art like none other I could name."
+
+Her face twitched as she tried to interpret that oddly formal praise - and your grim expression.`,
+
+`"Well, what is it then?"
+
+Her bright glass eyes shimmered in the forgelight.
+
+You had thought often that should you fail, you would find some way to spirit Nura away. Let her survive the Rosers, let her carry on the Art.
+
+A smith of her skill - let alone her knowledge of alchemy - would never want for work.
+
+She might have changed the world.`,
+
+`(You never had to do this.)`,
+
+`You made your choice.
+
+You walked to the rack of tinctures and potions, reached up and took an unmarked bottle from the highest shelf. Its contents gleamed, quicksilver-bright.
+
+"Nura, faithful apprentice." you said. "Do you believe we are working for good?"
+
+She did not hesitate. "Well of course! How could I doubt? Look at what the Rosers have done to our people! We have to stop them - we *will* stop them."
+
+You took a breath. "Then may I make a request of you?"`,
+
+`
+A look of bemusement, a short laugh.
+
+"Master, you make requests of me every damn day, you know that right?"
+
+You nodded, conceding. "True enough." Carefully, you placed the bottle on one of the workbenches.
+
+"Then please, would you drink this draught?"`,
+
+`She eyed it, curiosity warring with suspicion. "You're not going to tell me what it is?"
+
+"It is an alchemical concoction - a potent one, which I have held on to for a great many years. It will grant you certain abilities - abilities to contribute to our Great Work."
+
+Nura leaned, inspecting the vial. "There must be some reason why we have not used it already?"`,
+
+`You were committed. It was too late to change your mind.
+
+"I cannot be sure what you will experience, should you drink it. I am told that its great benefits are matched by equal dangers."
+
+(That was certainly one way of putting it.)
+
+She chuckled. "That's how it goes, with alchemy. No doubt the Rosers intend worse."
+
+You slid the bottle across the table. "Likely so. I can't make this choice for you, Azinureta - but I would not ask unless I did not think we can complete our Work without it."`,
+
+`Nura grimaced. "Doesn't sound like we have a choice then, Master. God protect me."
+
+She took the bottle, pulled out the stopper, and poured it down her throat.
+
+How does a homunculus drink? What strange organs lay under Nura's porcelain skin? You did not know, but it didn't matter.
+
+The effect was mercifully quick. Nura's body froze, before she had the chance to form a look of betrayal. Frozen - but not dead.`,
+
+`Her body was heavier than you imagined - but not too heavy for a seasoned smith to lift her to the furnace.
+
+The transformation would be all too familiar. Nigredo, albedo, citrinitas, rubedo: each sacrifice before had followed the pattern.
+
+Smoke rose from her core as the divisions and partitions inside her began to give way.
+
+No time to carve symbols: you had to work quickly, before too much of her leaked away.`,
+
+`The sword is a device for projecting a will into the world, over and against the will of others. The essence of Alchemy.
+
+The sword is a crystallisation of purpose. All that it encounters is absorbed and turned to its ultimate end.
+
+But until that moment, you were missing something. A way to start the process, to begin the cycle.
+
+Azinureta's body no longer resembled porcelain. A dark tangle - like smoke, like hair? What is the difference?`,
+
+`The alchemical brew had burned away all that was unnecessary. The quintessence remained.
+
+Its form was lost, but not forgotten.
+
+Ghost-white threads reached out into the air, as the will that held her body together gave way.
+
+You drew out the threads, spun them onto the sword. The rest of her body crumbled, lifeless grey ash.`,
+
+`You thrust the sword into the furnace, stoked the bellows, lifted it out to the anvil. The point where alchemy and smithwork met.
+
+Your hammer struck true, the air filling with gold. The threads of quintessence held the fire, a thousand candles of Nura.
+
+It was beautiful, in its way, to see your apprentice set aglow like this.`,
+
+`The tempering. A sword must be hard at its edge, but flexible at its core. Nura would have to be shaped the same way.
+
+Coat the sword in clay. Heat it, slowly, until it glows a rich red. Let the core cool slowly, the edge quickly.
+
+The threads of Nura, invisible now, would be shaped by the heat.
+
+(Could you hear me screaming? Would it have mattered?)`,
+
+`The present.
+
+"Nura..."
+
+(You don't get to call me that. Not anymore.)
+
+"Azinureta, I..."
+
+(Don't you dare say you're sorry.)`,
+
+`You do not. You stare ahead - wondering if she stands somewhere among these shards of building. Or is her presence of a more abstract from?
+
+"Then I will not. What's done is done. Take some measure of revenge from this, if you must - but here we are."`,
+
+`(How very practical. You would have made an *excellent* Roser.)
+
+Her contempt and anger curl in the air. So you say no more - let her have the last word.`,
+
+`You walk a few paces. See if there are boundaries to this un-place.
+
+As you step forwards, the buildings twist. For a second, they are the faces of a crowd of eager potentials, staring at you in shock as you named Nura as your apprentice.
+
+Don't think of her.`,
+
+`(I'm tempted to just let you keep walking. Walk until the futility crushes you like an insect.)
+
+The thought intrudes itself after some time. You have stepped on cobbles, flagstones, soil - even, for a brief instant, what you thought was manure. But when you looked down to check, it had already become a stained glass window.
+
+You stop. "But?"
+
+(But as much as I despise you, you are perhaps the one person who might be able to free me. Us.)
+
+You almost want to laugh.`,
+
+`"What would make you think that?"
+
+(You built this place, didn't you?)
+
+"I did."
+
+(Then you, of all people -)
+
+"You think this is just a jail, that we step out of the exit and all will be well?"`,
+
+`Silence. No thoughts rise unbidden - at least, none but your own.
+
+"We're fuel, Azinureta. That is all. There is nothing that *can* escape. Count your blessings that you may yet think, and remember."
+
+You don't bother to resume walking. It all seems rather futile, now.`,
+
+`What is happening outside the sword?
+
+You can remember sacrificing Azinureta - even the morning after, where you sobbed as you scraped up her ashes.
+
+You remember the dark irridescence of the finished sword, the glint of each symbol of sacrifice. The overwhelming feeling of urgency and purpose as you took it up to slide it, with a soft hiss, into the sheath.
+
+But what then?`,
+
+`To the point: who took up the sword, and killed you?
+
+Scenarios wander across your mind. Perhaps the Rosers infiltrated the castle. Perhaps you had enemies - was your murder of Nura witnessed?
+
+Perhaps you turned it on yourself.`];
