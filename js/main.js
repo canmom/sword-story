85ad855904765259b6423530f8c9c08459b31f7f
canmom <bryn.dickinson@gmail.com>
2019-07-04 22:25:49 +0100

    Add framework to support animations
    This includes:
     - replacing the background with an HTML5 canvas element
     - an asynchronous preloader for images
     - a class to represent an animation given a frame strip
     - drawing animations to the screen
     - a test with the first of several animations
    
    Remaining to be done:
     - loading a list of animation specs from a config file
     - initiating transitions & animations in the Twee script
     - making basic 'load paragraph' transition sexier

--- js/main.js
+++ js/main.js
@@ -1,12 +1,90 @@
+const imagePrefix = '/img/'
+
 const game = {
   narration : document.getElementById('narration'),
   currentParagraphs : document.getElementById('passages').childNodes,
+  context : document.getElementById('canvas').getContext('2d'),
+  offscreenBuffer : null,
   paragraphIndex : 0,
   passageIndex : 0,
   passageContainer: document.getElementById('passages'),
   narrationBox: document.getElementById('narration'),
-  passages: null,  
+  passages: null,
+  images: {},
+  animations: [],
 };
+
+class Animation {
+  // currentFrame = 0;
+  // framesImage;
+  // nFrames = 0;
+  // pos = {x: 0, y: 0};
+  // frameSize = {w: 0, h: 0}
+  // frameInterval = 0;
+  // loopInterval = 0;
+  // loopStyle = "basic";
+  // #rewinding = false;
+  // nextFrameTimestamp = 0;
+
+  constructor(frames, nFrames, pos, frameSize, frameInterval, loopInterval, loopStyle) {
+    this.framesImage = frames;
+    this.nFrames = nFrames;
+    this.pos = pos;
+    this.frameSize = frameSize;
+    this.frameInterval = frameInterval;
+    this.loopInterval = loopInterval;
+    this.loopStyle = loopStyle ? loopStyle : "basic";
+    this.rewinding = false;
+    this.nextFrameTimestamp = 0;
+    this.currentFrame = 0;
+    this.playing = true;
+  }
+
+  advanceFrame() {
+    if (!this.rewinding) {
+      if (this.currentFrame < this.nFrames - 1) {
+        this.currentFrame += 1;
+      } else if (this.loopStyle = "rewind") {
+        this.rewinding = true;
+        this.advanceFrame();
+      } else {
+        this.currentFrame = 0;
+        this.stopAndScheduleNextPlayback();
+      }
+    } else {
+      if (this.currentFrame > 0) {
+        this.currentFrame -= 1;
+      } else {
+        this.rewinding = false;
+        this.stopAndScheduleNextPlayback();
+      }
+    }
+  }
+
+  stopAndScheduleNextPlayback() {
+    this.playing = false;
+    window.setTimeout(() => {this.playing = true}, this.loopInterval);
+  }
+
+  draw(timestamp,context) {
+    if (this.playing) {
+      if (timestamp > this.nextFrameTimestamp) {
+        this.advanceFrame();
+        this.nextFrameTimestamp = timestamp + this.frameInterval;
+      }
+      context.drawImage(
+        this.framesImage,
+        this.currentFrame * this.frameSize.w,
+        0,
+        this.frameSize.w,
+        this.frameSize.h,
+        this.pos.x,
+        this.pos.y,
+        this.frameSize.w,
+        this.frameSize.h);
+    }
+  }  
+}
 
 function smarten(text) {
   return text
@@ -86,19 +164,111 @@ function showNextParagraph() {
   }
 }
 
+/*async game loader*/
 function loadScript() {
   return new Promise(function(resolve, reject) {
     fetch('twee/script.tw')
       .then(function (response) {
         response.text().then(function(text) {
           game.passages = text.split(/::[A-z '\[\]]+\n/).slice(1);
+          console.log(`Successfully loaded script containing ${game.passages.length} lines!`)
           resolve();
-      });
+      }, reject);
     });
   });
 }
 
-document.getElementById('nextbutton').addEventListener('click', showNextParagraph)
+function loadImage(url) {
+  return new Promise((resolve, reject) => {
+    let img = new Image();
+    img.addEventListener('load', e => resolve(img));
+    img.addEventListener('error', () => {
+      reject(new Error(`Failed to load image's URL: ${url}`));
+    });
+    img.src = url;
+  });
+}
 
+function loadImages(imageList) {
+  return new Promise((resolve, reject) => {
+    const numImagesToLoad = imageList.length;
+    imageList.forEach((imageName) => {
+      const imageUrl = imagePrefix + imageName;
+      loadImage(imageUrl).then(
+        (image) => {/*on resolve*/ 
+          game.images[imageName] = image
+          if (Object.keys(game.images).length === numImagesToLoad) {
+            console.log(`Successfully loaded ${numImagesToLoad} images!`)
+            resolve()
+          }
+        },
+        (error) => {/*on reject*/
+          reject(error)
+        }
+      );
+    });
+  });
+}
 
-loadScript().then(displayPassage);
+function testAnimation() {
+  game.animations.push(new Animation(
+    game.images['blink.png'],
+    5,
+    {x: 120, y: 190},
+    {w: 249, h: 226},
+    100,
+    2500,
+    "rewind"))
+}
+
+function loadGame() {
+  return new Promise((resolve, reject) => {
+    setUpDoubleBuffering();
+    document.getElementById('nextbutton').addEventListener('click', showNextParagraph);
+    const loadingState = {
+      images: false,
+      script: false,
+    };
+
+    function resolveIfReady() {
+      if (loadingState.images && loadingState.script) {
+        resolve()
+      }
+    }
+
+    loadScript().then(() => {
+      loadingState.script = true;
+      resolveIfReady();
+    });
+
+    loadImages(['background.png','blink.png']).then(() => {
+      loadingState.images = true;
+      resolveIfReady();
+    })
+  });
+}
+
+function drawFrame(timestamp) {
+  game.context.drawImage(game.images['background.png'],0,0);
+  // game.context.drawImage(game.images['blink.png'],0,0,249,226,100,100,249,226);
+  game.animations.forEach((animation) => {
+    animation.draw(timestamp,game.context);
+  });
+  /*game.context.drawImage(game.offscreenBuffer.canvas,0,0);*/
+  window.requestAnimationFrame(drawFrame)
+}
+
+function setUpDoubleBuffering() {
+  const buffer = document.createElement('canvas');
+  buffer.width = game.context.canvas.width;
+  buffer.height = game.context.canvas.height;
+  game.offscreenBuffer = buffer.getContext('2d');
+}
+
+function beginGame() {
+  testAnimation();
+  window.requestAnimationFrame(drawFrame);
+  displayPassage();
+}
+
+loadGame().then(beginGame);
